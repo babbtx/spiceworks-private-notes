5dfa744908f0b7895e5da188cad05f0a9af78fdd
David Babbitt <davidb@spiceworks.com>
2015-10-28 18:17:46 -0500

    simple note model in Parse

---
+++ source/javascripts/backbone/models/note.js
@@ -0,0 +1,40 @@
+App.module("Models", function(Models, App, Backbone, Marionette, $, _){
+  Models.Note = Parse.Object.extend({
+    className: "Note",
+  });
+
+  Models.Note.API = {
+
+    initialize: function(properties){
+      var note = new Models.Note();
+      _.each(properties || {}, function(value,property){
+        note.set(property, value);
+      });
+      return note;
+    },
+
+    findOrInitialize: function(properties){
+      var query = new Parse.Query(Models.Note);
+      _.each(properties || {}, function(value,property){
+        query.equalTo(property, value);
+      })
+      var d = $.Deferred();
+      query.first({
+        success: function(note){
+          if (!note){
+            d.resolve(Models.Note.API.initialize(properties));
+          }
+          else {
+            d.resolve(note);
+          }
+        },
+        error: function(error){
+          d.reject(error);
+        }
+      });
+      return d.promise();
+    }
+  };
+
+  App.channel.reply("models:note", Models.Note.API.findOrInitialize);
+});
