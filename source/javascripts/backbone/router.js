50f2a8e5779b25533531f915ced9eb8c649cbd47
David Babbitt <davidb@spiceworks.com>
2015-10-30 15:17:09 -0500

    rename and formatting

--- source/javascripts/backbone/router.js
+++ source/javascripts/backbone/router.js
@@ -6,33 +6,40 @@ App.module("Root", function(Root, App, Backbone, Marionette, $, _){
    * use the routes and not the controller method names, then sync the URL there.
    */
 
-  Root.Controller = {
-    "findNote": function(){
+  Root.ContainerController = {
+
+    findNote: function(){
       App.rootLayout.showChildView("container", new App.FindNote.Views.Main());
       // update URL
       Backbone.history.navigate("note/find", {trigger: false});
     },
-    "editNote": function(options){
+
+    editNote: function(options){
       App.rootLayout.showChildView("container", new App.EditNote.Views.Main(options));
       // update URL
       var key = options.key || options.model.get("key");
       Backbone.history.navigate("note/" + key, {trigger: false});
     },
-    "editNoteByKey": function(key){
+
+    editNoteByKey: function(key){
       this.editNote({key: key});
     },
+
+    notFound: function(){
+      window.location.href = "/404.html";
     }
   };
 
   Root.Router = Marionette.AppRouter.extend({
-    controller: Root.Controller,
+    controller: Root.ContainerController,
     appRoutes: {
       "note/find": "findNote",
       "note/:key": "editNoteByKey",
+      "*notFound": "notFound"
     }
   });
 
   App.channel.on("router:navigate", function(name /*...*/){
-    Root.Controller[name].apply(Root, _.toArray(arguments).slice(1));
+    Root.ContainerController[name].apply(Root, _.toArray(arguments).slice(1));
   });
 });
