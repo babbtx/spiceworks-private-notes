4898157c166441fcb179d19937deb5fde22dbb3d
David Babbitt <davidb@spiceworks.com>
2015-10-30 03:18:53 -0500

    ability to route to note by object and not just by key

--- source/javascripts/backbone/router.js
+++ source/javascripts/backbone/router.js
@@ -1,16 +1,26 @@
 App.module("Root", function(Root, App, Backbone, Marionette, $, _){
 
   Root.Controller = {
-    "editNote": function(key){
-      App.rootLayout.showChildView("container", new App.EditNote.Views.Main({key: key}));
+    "editNote": function(options){
+      App.rootLayout.showChildView("container", new App.EditNote.Views.Main(options));
+      // update URL
+      var key = options.key || options.model.get("key");
+      Backbone.history.navigate("note/" + key, {trigger: false});
+    },
+    "editNoteByKey": function(key){
+      this.editNote({key: key});
+    },
     }
   };
 
   Root.Router = Marionette.AppRouter.extend({
     controller: Root.Controller,
     appRoutes: {
-      "note/:key": "editNote"
+      "note/:key": "editNoteByKey",
     }
   });
 
+  App.channel.on("router:navigate", function(name /*...*/){
+    Root.Controller[name].apply(Root, _.toArray(arguments).slice(1));
+  });
 });
