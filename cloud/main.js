20867f822e1cf1c1536ead024469820cf1a051a6
David Babbitt <davidb@spiceworks.com>
2015-11-01 16:42:36 -0600

    main cloud code routine for logging in with spiceworks login. verify the login token against the server and then login/create users

---
+++ cloud/main.js
@@ -0,0 +1,17 @@
+var authorizer = require("lib/spiceworks_authorizer");
+var spiceworks_user = require("lib/spiceworks_user");
+
+Parse.Cloud.define("getLoginToken", function(request, response){
+  authorizer.authorize(request.params)
+    .then(function(){
+      return spiceworks_user.loginOrCreate(request.params.user_auid);
+    })
+    .then(function(user){
+      response.success(user.getSessionToken());
+    })
+    .fail(function(err){
+      var message = (typeof err === "string") ? err : JSON.stringify(err);
+      console.warn("Returning login error to browser: " + message);
+      response.error(message);
+    })
+});
