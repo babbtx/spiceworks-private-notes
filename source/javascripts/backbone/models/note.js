5766425d37708ef2d076866a17614bf8b74df0e6
David Babbitt <davidb@spiceworks.com>
2015-11-02 12:59:37 -0600

    create a Backbone.Collection of parse Note objects. total hacking.

--- source/javascripts/backbone/models/note.js
+++ source/javascripts/backbone/models/note.js
@@ -1,11 +1,26 @@
 App.module("Models", function(Models, App, Backbone, Marionette, $, _){
+  var count = 0;
+
   Models.Note = Parse.Object.extend({
     className: "Note",
 
     initialize: function(attrs, options){
+      this.cid = "Note-" + ++count; // necessary to stuff these into the collection below
       _.extend(this, Models.ParseEvents);
     }
   });
+
+  Models.NotesCollection = Backbone.Collection.extend({
+    // Backbone.Collection wants the model id from the object attributes
+    // but Parse stores the attributes as a separate object from the object.id.
+    // Luckily we can use our "key" attribute as an object id in the collection.
+    modelId: function(attrs){
+      return attrs.key;
+    },
+    _isModel: function(obj){
+      return (obj instanceof Models.Note);
+    },
+  })
 
   Models.Note.API = {
 
@@ -53,9 +68,29 @@ App.module("Models", function(Models, App, Backbone, Marionette, $, _){
               d.reject(err);
             });
           return d.promise();
-        })
+        });
+    },
+
+    list: function(){
+      return App.channel.request("login")
+        .then(function(){
+          var d = $.Deferred();
+          var query = new Parse.Query(Models.Note);
+          query.equalTo("user", Parse.User.current());
+          query.descending("updatedAt");
+          query.limit(5);
+          query.find()
+            .done(function(list){
+              d.resolve(new Models.NotesCollection(list || []));
+            })
+            .fail(function(err){
+              d.reject(err);
+            });
+          return d.promise();
+        });
     }
   };
 
   App.channel.reply("models:note", Models.Note.API.findOrInitialize);
+  App.channel.reply("models:note:list", Models.Note.API.list);
 });
