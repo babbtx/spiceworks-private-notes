34631dca54fc0be63bc9941161ff1c411914feb9
David Babbitt <davidb@spiceworks.com>
2015-11-02 02:29:03 -0600

    added force login function. turns out i didn't use it this way.

--- source/javascripts/backbone/apps/login/login.js
+++ source/javascripts/backbone/apps/login/login.js
@@ -23,25 +23,41 @@ App.module("Login", function(Login, App, Backbone, Marionette, $, _) {
       });
     },
 
-    login: function(){
+    autoLoginWithUI: function(){
       var that = this;
-      Parse.Session.current()
-        .done(function(){
-          return $.when().promise(); // resolved
-        })
-        .fail(function(){
-          return $.when()
-            .then(that.showLoginModal.bind(that))
-            .then(that.autoLoginUser.bind(that))
-            .then(that.dismissLoginModal.bind(that))
-            .fail(that.showLoginFailureModal.bind(that));
-        });
+      return $.when()
+        .then(that.showLoginModal.bind(that))
+        .then(that.autoLoginUser.bind(that))
+        .then(that.dismissLoginModal.bind(that))
+        .fail(that.showLoginFailureModal.bind(that));
+    },
+
+    login: function(force){
+      if (force) {
+        return this.autoLoginWithUI();
+      }
+      else {
+        var that = this;
+        var d = $.Deferred();
+        Parse.Session.current()
+          .done(function(){ // session's Parse.Promise
+            d.resolve();
+          })
+          .fail(function(){ // session's Parse.Promise
+            that.autoLoginWithUI()
+              .done(function(){ // autoLoginWithUI's $.Deferred
+                d.resolve();
+              })
+              // not propagating failure here because autoLoginWithUI pops the error modal on failure
+          });
+        return d.promise();
+      }
     }
 
   };
 
-  App.channel.reply("login", function(){
-    return Login.Controller.login();
+  App.channel.reply("login", function(force){
+    return Login.Controller.login(force);
   });
 
 });
